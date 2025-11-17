c01fac314e8c711184d2a503c897784cf63bba99
delisha <delishanaik02@gmail.com>
2025-11-17 16:19:46 +0530

    Redirect Home in Navbar to SimPPL website

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -26,7 +26,7 @@ export default function Navbar() {
 
           <div className="hidden md:flex space-x-8">
             <Link
-              href="/"
+              href="https://simppl.org/"
               className="text-gray-700 hover:text-gray-900 transition-colors"
             >
               Home
@@ -76,7 +76,7 @@ export default function Navbar() {
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 bg-white border-t border-gray-100">
               <Link
-                href="/"
+                href="https://simppl.org/"
                 className="block px-3 py-2 text-gray-700 hover:text-gray-900"
                 onClick={() => setIsOpen(false)}
               >
